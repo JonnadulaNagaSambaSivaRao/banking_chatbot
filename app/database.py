fae3ce83c937d15eb4bab74274e6d01bb970f18f
Sambasivarao <sivajonnadula99@gmail.com>
2026-08-01 02:23:18 +0530

    Change database URL from SQLite to MySQL

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL="sqlite:///./banking.db"
-
+DATABASE_URL = "mysql+pymysql://username:password@localhost/banking_chatbot"
 engine=create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread":False}
